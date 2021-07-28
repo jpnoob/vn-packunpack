aa9b30e368484d7149cbed75131b3f67613fab05
jpnoob <baka@superruben.com>
2021-07-28 20:39:56 +0200

    added proper skipping of non-script

--- BGI/bgidecode.c
+++ BGI/bgidecode.c
@@ -6,10 +6,10 @@
    having to explicitly specify (or redirect) output is a safety measure
    to avoid decoding and overwriting an existing decoded file by accident
 
-   TODO: add better support for not extracting non-script text
-
    v1.0: initial version
    v1.1: added support for format 0x10 (daitoshokan trial).
+   v1.2: added very ugly support for not extracting non-script text
+   v1.3: added proper support for not extracting non-script text
 */
 
 #include <stdio.h>
@@ -19,10 +19,10 @@
 #include <stdbool.h>
 
 uint32_t opt_format;      // script format, 0x01 (newer games) or 0x10 (older games)
-bool opt_nocommand=false; // true: attempt to remove commands not part of script
+bool opt_nocommand=false; // true: remove commands not part of script
 
 void usage() {
-	fputs("bgidecode v1.1 by me in 2021\n\n",stderr);
+	fputs("bgidecode v1.3 by me in 2021\n\n",stderr);
 	fputs("usage: bgidecode [-s] script-file\n\n",stderr);
 	fputs("-s: skip extraction of non-script text\n",stderr);
 	fputs("output is sent to stdout, redirect it yourself\n",stderr);
@@ -38,61 +38,10 @@ void error(char *s) {
 #define MAXLEN 10000000
 unsigned char file[MAXLEN];
 
-// pretty arbitraty list of commands taken from hajirabu
-char command[][50]={
-	"_BS5HExit",
-	"_ChangeVolumeOfStreamSE",
-	"_DrawBS",
-	"_DrawScene",
-	"_DrawSPR",
-	"_FadeBGM",
-	"_FadeScene",
-	"_FadeSPR",
-	"_Font",
-	"_MoveRepeatBS",
-	"_PlayBGM",
-	"_PlaySE",
-	"_PlayStreamSE",
-	"_PlayVoice",	
-	"_ScrollBG",
-	"_Wait",
-	"bg",
-	"bgm0",
-	"black",
-	"bustshot4",
-	"call",
-	"clear",
-	"fast_cutin",
-	"font",
-	"jump",
-	"return",
-	"scene",
-	"se0",
-	"sprite7",
-	"sprite8",
-	"streamse2",
-	"voice0",
-	"wait",
-	"white",
-	"BLACK",
-	"CallScriptForFButton",
-	"EnableWindow",
-	"FButtonWait",
-	"LABEL_END",
-	"Scissors_01",
-	"SoundStopAll",
-	"StartShakingOfWindowSx",
-	"Wait",
-	""
-};
-
-// pretty arbitraty list of commands (substring matching) taken from hajirabu
-char contain[][50]={
-	"BURIKOScriptSystem",
-	"Emphasis_",
-	"PrintMessage",
-	""
-};
+uint32_t name172[]={0,-1,0x3f,2,0xfe,0,1,0,1,0,0,3};
+uint32_t text172[]={0,-1,0x3f,2,0xfe,0,1,0,1,0,0,-1,-1,3};
+uint32_t name169[]={0,1,0,1,0,0,3};
+uint32_t text169[]={0,1,0,1,0,0,-1,-1,3};
 
 uint32_t getuint32(unsigned char *p) {
 	return p[0]+(p[1]<<8)+(p[2]<<16)+(p[3]<<24);
@@ -152,11 +101,44 @@ void decode(char *in) {
 			// only accept strings with pointer > strptr
 			if(str>=strptr && ptr<str) {
 				if(earlieststring>str) earlieststring=str;
-				// filter away known commands
-				for(int i=0;command[i][0];i++) if(!strcmp((char *)file+str,command[i])) goto skip;
-				for(int i=0;contain[i][0];i++) if(strstr((char *)file+str,contain[i])) goto skip;
-				printf("<%u,%u,%zd>%s\n",ptr-offs,str-offs,strlen((char *)file+str),file+str);
-			skip:;
+				bool isnametext=true;
+				if(opt_nocommand) {
+					// only accept strings used for character and text
+					int i,j;
+					isnametext=false;
+					if(opt_format==1) {
+						// check if current string is name in v1.72
+						for(i=-0x30,j=0;i<0;i+=4,j++) {
+							if(ptr+i<0) break;
+							if(name172[j]<0xffffffffu && getuint32(file+ptr+i)!=name172[j]) break;
+						}
+						if(!i) isnametext=true;
+						// check if current string is text in v1.72
+						for(i=-0x38,j=0;i<0;i+=4,j++) {
+							if(ptr+i<0) break;
+							if(text172[j]<0xffffffffu && getuint32(file+ptr+i)!=text172[j]) break;
+						}
+						if(!i) isnametext=true;
+					} else if(opt_format==0x10) {
+						// check if current string is name in v1.69
+						for(i=-0x1c,j=0;i<0;i+=4,j++) {
+							if(ptr+i<0) break;
+							if(name169[j]<0xffffffffu && getuint32(file+ptr+i)!=name169[j]) break;
+						}
+						if(!i) isnametext=true;
+						// check if current string is text in v1.69
+						for( i=-0x24,j=0;i<0;i+=4,j++) {
+							if(ptr+i<0) break;
+							if(text169[j]<0xffffffffu && getuint32(file+ptr+i)!=text169[j]) break;
+						}
+						if(!i) isnametext=true;
+					}
+				}
+				if(isnametext) {
+					// skip line consisting of just 81 40 (japanese space) that slipped through
+					if(!(opt_nocommand && file[str]==0x81 && file[str+1]==0x40 && !file[str+2]))
+						printf("<%u,%u,%zd>%s\n",ptr-offs,str-offs,strlen((char *)file+str),file+str);
+				}
 			}
 		}
 		ptr+=4;
